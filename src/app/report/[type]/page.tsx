5e935cb62d2eea7bbc97236c34fd16d870434e8f
Akram <aliakram9789@gmail.com>
2024-01-24 09:27:16 +0530

    calorie intake popup fixed

--- src/app/report/[type]/page.tsx
+++ src/app/report/[type]/page.tsx
@@ -8,8 +8,8 @@ import { usePathname } from "next/navigation";
 
 const page = () => {
   const color = "#ffc20e";
-  const pathName = usePathname()
-  console.log(pathName)
+  const pathName = usePathname();
+  console.log(pathName);
 
   const chartsParams = {
     // margin: { bottom: 20, left: 25, right: 5 },
@@ -19,80 +19,117 @@ const page = () => {
   const [dataS1, setDataS1] = React.useState<any>(null);
 
   const getDataForS1 = async () => {
-    let temp = [
-      {
-        date: "Thu Sep 28 2023 20:30:30 GMT+0530 (India Standard Time)",
-        value: 2000,
-        unit: "kcal",
-      },
-      {
-        date: "Wed Sep 27 2023 20:30:30 GMT+0530 (India Standard Time)",
-        value: 2500,
-        unit: "kcal",
-      },
-      {
-        date: "Tue Sep 26 2023 20:30:30 GMT+0530 (India Standard Time)",
-        value: 2700,
-        unit: "kcal",
-      },
-      {
-        date: "Mon Sep 25 2023 20:30:30 GMT+0530 (India Standard Time)",
-        value: 3000,
-        unit: "kcal",
-      },
-      {
-        date: "Sun Sep 24 2023 20:30:30 GMT+0530 (India Standard Time)",
-        value: 2000,
-        unit: "kcal",
-      },
-      {
-        date: "Sat Sep 23 2023 20:30:30 GMT+0530 (India Standard Time)",
-        value: 2300,
-        unit: "kcal",
-      },
-      {
-        date: "Fri Sep 22 2023 20:30:30 GMT+0530 (India Standard Time)",
-        value: 2500,
-        unit: "kcal",
-      },
-      {
-        date: "Thu Sep 21 2023 20:30:30 GMT+0530 (India Standard Time)",
-        value: 2700,
-        unit: "kcal",
-      },
-    ];
+    // let temp = [
+    //   {
+    //     date: "Thu Sep 28 2023 20:30:30 GMT+0530 (India Standard Time)",
+    //     value: 2000,
+    //     unit: "kcal",
+    //   },
+    //   {
+    //     date: "Wed Sep 27 2023 20:30:30 GMT+0530 (India Standard Time)",
+    //     value: 2500,
+    //     unit: "kcal",
+    //   },
+    //   {
+    //     date: "Tue Sep 26 2023 20:30:30 GMT+0530 (India Standard Time)",
+    //     value: 2700,
+    //     unit: "kcal",
+    //   },
+    //   {
+    //     date: "Mon Sep 25 2023 20:30:30 GMT+0530 (India Standard Time)",
+    //     value: 3000,
+    //     unit: "kcal",
+    //   },
+    //   {
+    //     date: "Sun Sep 24 2023 20:30:30 GMT+0530 (India Standard Time)",
+    //     value: 2000,
+    //     unit: "kcal",
+    //   },
+    //   {
+    //     date: "Sat Sep 23 2023 20:30:30 GMT+0530 (India Standard Time)",
+    //     value: 2300,
+    //     unit: "kcal",
+    //   },
+    //   {
+    //     date: "Fri Sep 22 2023 20:30:30 GMT+0530 (India Standard Time)",
+    //     value: 2500,
+    //     unit: "kcal",
+    //   },
+    //   {
+    //     date: "Thu Sep 21 2023 20:30:30 GMT+0530 (India Standard Time)",
+    //     value: 2700,
+    //     unit: "kcal",
+    //   },
+    // ];
 
-    let dataForLineChart = temp.map((item: any) => {
-      let val = JSON.stringify(item.value);
-      return val;
-    });
+    if (pathName == "/report/Calorie%20Intake") {
+      try {
+        const response = await fetch(
+          process.env.NEXT_PUBLIC_BACKEND_API +
+            "/calorieintake/getcalorieintakebylimit",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            credentials: "include",
+            body: JSON.stringify({
+              limit: 10,
+            }),
+          }
+        );
 
-    let dataForXAxis = temp.map((item: any) => {
-      let val = new Date(item.date);
-      return val;
-    });
+        const data = await response.json();
+        if (response.ok) {
+          let temp = data.data.map((item: any) => {
+            return {
+              date: item.date,
+              value: item.calorieIntake,
+              unit: "kcal",
+            };
+          });
 
-    console.log({
-      data: dataForLineChart,
-      title: "1 Day Calorie Intake",
-      color: color,
-      xAxis: {
-        data: dataForXAxis,
-        label: "Last 10 Days",
-        scaleType: "time",
-      },
-    });
 
-    setDataS1({
-      data: dataForLineChart,
-      title: "1 Day Calorie Intake",
-      color: color,
-      xAxis: {
-        data: dataForXAxis,
-        label: "Last 10 Days",
-        scaleType: "time",
-      },
-    });
+            let dataForLineChart = temp.map((item: any) => {
+              let val = JSON.stringify(item.value);
+              return val;
+            });
+
+            let dataForXAxis = temp.map((item: any) => {
+              let val = new Date(item.date);
+              return val;
+            });
+
+            console.log({
+              data: dataForLineChart,
+              title: "1 Day Calorie Intake",
+              color: color,
+              xAxis: {
+                data: dataForXAxis,
+                label: "Last 10 Days",
+                scaleType: "time",
+              },
+            });
+
+            setDataS1({
+              data: dataForLineChart,
+              title: "1 Day Calorie Intake",
+              color: color,
+              xAxis: {
+                data: dataForXAxis,
+                label: "Last 10 Days",
+                scaleType: "time",
+              },
+            });
+        } else {
+          setDataS1([]);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      alert("Get Data for other reports");
+    }
   };
 
   React.useEffect(() => {
@@ -116,14 +153,14 @@ const page = () => {
                 valueFormatter: (date: any) => {
                   return date.getDate().toString();
                 },
-              },
+              }
             ]}
             series={[
               {
                 data: dataS1.data,
                 label: dataS1.title,
                 color: dataS1.color,
-              },
+              }
             ]}
             {...chartsParams}
           />
@@ -132,10 +169,10 @@ const page = () => {
       <button
         className="editbutton"
         onClick={() => {
-          if (pathName == "/report/Calorie%20Intake"){
+          if (pathName == "/report/Calorie%20Intake") {
             setShowCalorieIntakePopup(true);
-          }else{
-            alert("show other popups")
+          } else {
+            alert("show other popups");
           }
         }}
       >
--- src/components/ReportFormPopup/CalorieIntake/CalorieIntakePopup.tsx
+++ src/components/ReportFormPopup/CalorieIntake/CalorieIntakePopup.tsx
@@ -3,7 +3,7 @@ import "../popup.css";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { AiFillDelete, AiOutlineClose } from "react-icons/ai";
-import {TimePicker} from "@muli/x-date-pickers/TimePicker"
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
 import dayjs, { Dayjs } from "dayjs";
 import { DatePicker } from "@mui/x-date-pickers";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
@@ -23,43 +23,142 @@ const CalorieIntakePopup: React.FC<CaloriIntakePopupProps> = ({
   const [time, setTime] = React.useState<any>(dayjs(new Date()));
 
   const selectedDay = (val: any) => {
-    setDate(val)
+    setDate(val);
   };
 
-  const [calorieIntake, setCalorieIntake ] = React.useState<any>({
-    item:'',
-    date:'',
-    quantity:'',
-    quantityType:''
-  })
+  const [calorieIntake, setCalorieIntake] = React.useState<any>({
+    item: "",
+    date: "",
+    quantity: "",
+    quantityType: "g",
+  });
 
-  const [item, setItem] = React.useState<any>([])
+  const [items, setItems] = React.useState<any>([]);
 
-  const saveCalorieIntake = async () =>{
-    
-  }
-  const getCalorieIntake = async () =>{
+  const saveCalorieIntake = async () => {
+    let tempdate = date.format("YYYY-MM-DD");
+    let temptime = time.format("HH:mm:ss");
+    let tempdatetime = tempdate + " " + temptime;
+    let finaldatetime = new Date(tempdatetime);
+    console.log(calorieIntake);
 
-  }
-  const deleteCalorieIntake = async (item:any) =>{
+    try {
+      const response = await fetch(
+        process.env.NEXT_PUBLIC_BACKEND_API + "/calorieintake/addcalorieintake",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+          body: JSON.stringify({
+            item: calorieIntake.item,
+            date: finaldatetime,
+            quantity: calorieIntake.quantity,
+            quantitytype: calorieIntake.quantityType,
+          }),
+        }
+      );
 
-  }
+      const data = await response.json();
+      if (response.ok) {
+        toast.success("Calorie Intake added successfully");
+        getCalorieIntake();
+      } else {
+        toast.error("Error in adding calorie intake");
+        console.log(data);
+      }
+    } catch (error) {
+      toast.error("Error in adding calorie intake in database");
+      console.log(error);
+    }
+  };
 
-  React.useEffect(()=>{
-    getCalorieIntake()
-  },[date])
+  const getCalorieIntake = async () => {
+    setItems([]);
+    try {
+      const response = await fetch(
+        process.env.NEXT_PUBLIC_BACKEND_API +
+          "/calorieintake/getcalorieintakebydate",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+          body: JSON.stringify({
+            date: date,
+          }),
+        }
+      );
+
+      const data = await response.json();
+      if (data.ok) {
+        console.log(data.data, "calorie Intake data data for data");
+        setItems(data.data);
+      } else {
+        toast.error("Error in calorie intake");
+        console.log(data);
+      }
+    } catch (error) {
+      toast.error("Error in adding calorie intake in database");
+      console.log(error);
+    }
+  };
+
+  const deleteCalorieIntake = async (item: any) => {
+    try {
+      const response = await fetch(
+        process.env.NEXT_PUBLIC_BACKEND_API +
+          "/calorieintake/deletecalorieintake",
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+          body: JSON.stringify({
+            item:item.item,
+            date:item.date
+          }),
+        }
+      );
+
+      const data = await response.json();
+      if (data.ok) {
+        console.log(data.data, "calorie Intake data data for data");
+        toast.success("Calorie Intake item deleted successfully")
+        getCalorieIntake()
+      } else {
+        toast.error("Error in deleting calorie intake");
+        console.log(data);
+      }
+    } catch (error) {
+      toast.error("Error in deleting calorie intake in database");
+      console.log(error);
+    }
+  };
+
+  React.useEffect(() => {
+    getCalorieIntake();
+  }, [date]);
 
   return (
     <div className="popupout">
       <div className="popupbox">
-        <button className="close" onClick={()=>{setShowCalorieIntakePopup(false)}}>
-          <AiOutlineClose/>
+        <button
+          className="close"
+          onClick={() => {
+            setShowCalorieIntakePopup(false);
+          }}
+        >
+          <AiOutlineClose />
         </button>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <DatePicker
             label="select the day"
-            onChange={(newValue:any)=>{
-              selectedDay(newValue)
+            onChange={(newValue: any) => {
+              selectedDay(newValue);
             }}
           />
         </LocalizationProvider>
@@ -68,12 +167,12 @@ const CalorieIntakePopup: React.FC<CaloriIntakePopupProps> = ({
           label="Food Item Name"
           variant="outlined"
           color="warning"
-          onChange={((e)=>{
+          onChange={(e) => {
             setCalorieIntake({
               ...calorieIntake,
-              item:e.target.value
-            })
-          })}
+              item: e.target.value,
+            });
+          }}
         />
         <TextField
           id="outlined-basic"
@@ -81,24 +180,44 @@ const CalorieIntakePopup: React.FC<CaloriIntakePopupProps> = ({
           type="number"
           variant="outlined"
           color="warning"
-          onChange={((e)=>{
+          onChange={(e) => {
             setCalorieIntake({
               ...calorieIntake,
-              quantity:e.target.value
-            })
-          })}
+              quantity: e.target.value,
+            });
+          }}
         />
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <TimePicker
-            lable="Pick the time"
+            label="Pick the time"
             value={time}
             onChange={(newValue) => setTime(newValue)}
           />
         </LocalizationProvider>
-        <Button variant="contained" colors="warning"
-        onclick={saveCalorieIntake}>
+        <Button variant="contained" onClick={saveCalorieIntake}>
           Save
         </Button>
+        <div className="hrline"></div>
+        <div className="items">
+          {items.map((item: any) => {
+            return (
+              <div className="item">
+                <div>{item.item}</div>
+                <div>
+                  {item.quantity} {item.quantitytype}
+                </div>
+                <button
+                  onClick={() => {
+                    deleteCalorieIntake(item);
+                  }}
+                >
+                  Delete
+                  <AiFillDelete />
+                </button>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
