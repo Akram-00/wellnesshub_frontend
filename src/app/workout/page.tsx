69033969394878c9639c91b6291cdab3e80baf42
Akram <aliakram9789@gmail.com>
2024-02-26 10:55:07 +0530

    comments removed

--- src/app/workout/page.tsx
+++ src/app/workout/page.tsx
@@ -9,43 +9,6 @@ const page = () => {
   const searchParams = useSearchParams();
   const workoutid = searchParams.get("id");
   const getWorkout = async () => {
-    // let data: any = {
-    //   type: "Chest",
-    //   imageUrl:
-    //     "https://images.unsplash.com/photo-1571019614242-c5c5dee9f50b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80",
-    //   durationInMin: 30,
-    //   exercises: [
-    //     {
-    //       exercise: "Flat Bench Press",
-    //       imageUrl: "https://gymvisual.com/img/p/1/7/5/5/2/17552.gif",
-    //       sets: 3,
-    //       reps: 10,
-    //       rest: 60,
-    //       description:
-    //         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-    //     },
-    //     {
-    //       exercise: "Incline Bench Press",
-    //       imageUrl: "https://gymvisual.com/img/p/1/0/3/9/8/10398.gif",
-    //       sets: 3,
-    //       reps: 10,
-    //       rest: 60,
-    //       description:
-    //         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-    //     },
-    //     {
-    //       exercise: "Decline Bench Press",
-    //       imageUrl: "https://gymvisual.com/img/p/6/5/2/3/6523.gif",
-    //       sets: 3,
-    //       reps: 10,
-    //       rest: 60,
-    //       description:
-    //         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
-    //     },
-    //   ],
-    // };
-
-    // setWorkout(data);
     try {
       const response = await fetch(
         process.env.NEXT_PUBLIC_BACKEND_API +
